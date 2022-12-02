b792d6c5814c86589f061b40740eba0078bf569d
Nikita <nikit.zakharov@gmail.com>
2022-12-02 22:51:51 +0300

    starting structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+abstract class Delivery
+{
+    public string Address;
+}
+
+class HomeDelivery : Delivery
+{
+    /* ... */
+}
+
+class PickPointDelivery : Delivery
+{
+    /* ... */
+}
+
+class ShopDelivery : Delivery
+{
+    /* ... */
+}
+
+class Order<TDelivery,
+TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    // ... Другие поля
+}
